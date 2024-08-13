540238b11ee38f308198d44551f35dce6de6b006
Bharat Bardiya <bharatbardiya123@gmail.com>
2024-08-13 12:45:05 +0530

    form and annual data calculated using @input(), @output()

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentData } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentData>()
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit(){
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    })
+    this.enteredInitialInvestment = '0';
+    this.enteredAnnualInvestment = '0';
+    this.enteredExpectedReturn = '5';
+    this.enteredDuration = '10';
+  }
+
+}
